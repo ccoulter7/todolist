7fc845247e8c2dd94721b565432515585cb7e542
40236583 <ccoulter17@qub.ac.uk>
2023-02-20 21:51:37 +0000

    updated todo list

--- src/App.js
+++ src/App.js
@@ -5,14 +5,29 @@ import Form from './components/Form';
 import ToDoList from './components/ToDoList';
 
 function App() {
+  // States
   const [inputText, setInputText] = useState("");
+  const [todos, setTodos] = useState([]);
+  const [status, setStatus] = useState("all");
+  const [filteredTodos, setFilteredTodos] = useState([])
+  // Functions
+  const filterHandler = () => {
+
+  }
+
   return (
     <div className="App">
       <header>
-        <h1> Cathal's Todo List</h1>
+        <h1> Todo List </h1>
       </header>
-      <Form setInputText={setInputText} />
-      <ToDoList />
+      <Form
+        todos={todos}
+        setTodos={setTodos}
+        setInputText={setInputText}
+        inputText={inputText}
+        setStatus={setStatus}
+      />
+      <ToDoList setTodos={setTodos} todos={todos} />
     </div>
   );
 }
--- src/components/ToDoList.js
+++ src/components/ToDoList.js
@@ -1,10 +1,21 @@
 import React from "react"
+// import components
+import Todo from './Todo'
 
 
-const ToDoList = () => {
+const ToDoList = ({ todos, setTodos }) => {
+
     return (
         <div className="todo-container">
             <ul className="todo-list"></ul>
+            {todos.map(todo => (
+                <Todo
+                    key={todo.id}
+                    text={todo.text}
+                    setTodos={setTodos}
+                    todos={todos}
+                    todo={todo} />
+            ))}
         </div>
     )
 }
