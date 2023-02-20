c52150a50ddbc41b9da4173acc0aef4c4f5d22fc
40236583 <ccoulter17@qub.ac.uk>
2023-02-20 12:06:39 +0000

    Added setinputtext prop to form

--- src/App.js
+++ src/App.js
@@ -1,14 +1,22 @@
+import React, { useState } from 'react';
 import './App.css';
-import React from 'react';
+// importing components
+import Form from './components/Form';
+import ToDoList from './components/ToDoList';
 
 function App() {
+  const [inputText, setInputText] = useState("");
   return (
     <div className="App">
       <header>
         <h1> Cathal's Todo List</h1>
       </header>
+      <Form setInputText={setInputText} />
+      <ToDoList />
     </div>
   );
 }
 
 export default App;
+
+
